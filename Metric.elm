3a5b508a48f36ba3204f61eb6532d3040fc4dd97
Jasper Woudenberg <mail@jasperwoudenberg.com>
2017-04-14 21:57:28 +0200

    Some nice aliases and all S.I. prefixes

--- Metric.elm
+++ Metric.elm
@@ -44,7 +44,10 @@ type alias Speed =
 
 -- Helpers
 
-type alias Constructor unit = Float -> Number unit
+
+type alias Constructor unit =
+    Float -> Number unit
+
 
 number : unit -> Constructor unit
 number unit value =
@@ -83,14 +86,104 @@ newton =
 -- Prefixes
 
 
-kilo : Prefix unit
-kilo =
-    prefix 1000
+yocto : Prefix unit
+yocto =
+    prefix 1.0e-24
+
+
+zepto : Prefix unit
+zepto =
+    prefix 1.0e-21
+
+
+atto : Prefix unit
+atto =
+    prefix 1.0e-18
+
+
+femto : Prefix unit
+femto =
+    prefix 1.0e-15
+
+
+pico : Prefix unit
+pico =
+    prefix 1.0e-12
+
+
+nano : Prefix unit
+nano =
+    prefix 1.0e-9
+
+
+micro : Prefix unit
+micro =
+    prefix 1.0e-6
 
 
 milli : Prefix unit
 milli =
-    prefix 0.001
+    prefix 1.0e-3
+
+
+centi : Prefix unit
+centi =
+    prefix 1.0e-2
+
+
+deci : Prefix unit
+deci =
+    prefix 1.0e-1
+
+
+deka : Prefix unit
+deka =
+    prefix 1.0e1
+
+
+hecto : Prefix unit
+hecto =
+    prefix 1.0e2
+
+
+kilo : Prefix unit
+kilo =
+    prefix 1.0e3
+
+
+mega : Prefix unit
+mega =
+    prefix 1.0e6
+
+
+giga : Prefix unit
+giga =
+    prefix 1.0e9
+
+
+tera : Prefix unit
+tera =
+    prefix 1.0e12
+
+
+peta : Prefix unit
+peta =
+    prefix 1.0e15
+
+
+exa : Prefix unit
+exa =
+    prefix 1.0e18
+
+
+zetta : Prefix unit
+zetta =
+    prefix 1.0e21
+
+
+yotta : Prefix unit
+yotta =
+    prefix 1.0e24
 
 
 
@@ -140,13 +233,16 @@ smul scalar (Number value unit) =
 (.*) =
     mul
 
+
 (./) : Number a -> Number b -> Number (Div a b)
 (./) =
     div
 
-(..) : Float -> Number unit -> Number unit
-(..) =
+
+(.!) : Float -> Number unit -> Number unit
+(.!) =
     smul
+
 
 
 -- Example use
